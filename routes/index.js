c43695318f2051edb82be707873bfaa617e54804
mosesmrima <mrimamss@gmail.com>
2021-09-21 01:26:28 +0300

    route

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router.get('/status', ((request, response) => AppController.getStatus(request, response)));
 router.get('/stats', ((request, response) => AppController.getStats(request, response)));
 router.post('/users', ((request, response) => UsersController.postNew(request, response)));
-router.get("/connect", ((request, response) => AuthController.getConnect(request, response)));
-router.get("/disconnect", ((request, response) => AuthController.getDisconnect(request, response)));
-router.get("/users/me", ((request, response) => UserController.getMe(request, response)));
+router.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
+router.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
+router.get('/users/me', ((request, response) => UsersController.getMe(request, response)));
 
 export default router;
