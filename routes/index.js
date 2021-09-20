f09c035942869c5b60db865806de2ed84c2d20da
mosesmrima <mrimamss@gmail.com>
2021-09-20 22:31:32 +0300

    routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
-
 router.get('/status', ((request, response) => AppController.getStatus(request, response)));
 router.get('/stats', ((request, response) => AppController.getStats(request, response)));
 router.post('/users', ((request, response) => UsersController.postNew(request, response)));
