7ea31281b86f8b83b79beeeb0d3c0269f82ffe2f
mosesmrima <mrimamss@gmail.com>
2021-09-21 01:25:48 +0300

    retrieve user by token

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,8 @@
 import sha1 from 'sha1';
 import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
+
+const { ObjectId } = require('mongodb');
 
 class UsersController {
   static async postNew(request, response) {
@@ -16,6 +19,21 @@ class UsersController {
 
     const result = await DBClient.db.collection('users').insertOne({ email: newEmail, password: passwordHash });
     return response.status(201).send({ id: result.insertedId, email: newEmail });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+
+    if (!token) return response.status(401).send({ error: 'Unauthorized1' });
+
+    const userId = await RedisClient.get(`auth_${token}`);
+    if (!userId) return response.status(401).send({ error: 'Unauthorized2' });
+
+    const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) return response.status(401).send({ error: 'Unauthorized3' });
+
+    delete user.password;
+    return response.status(200).send({ id: user._id, email: user.email });
   }
 }
 
